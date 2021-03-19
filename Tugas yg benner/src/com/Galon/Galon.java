1f03c6e2543a05a6e0d0c294620a5957f48bed65
137Anugrah <80816710+137Anugrah@users.noreply.github.com>
2021-03-19 19:05:57 +0700

    Add files via upload

---
+++ Tugas yg benner/src/com/Galon/Galon.java
@@ -0,0 +1,16 @@
+package com.Galon;
+import java.util.Scanner;
+public class Galon {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double galon, total;
+        System.out.println("Konversi Galon ke Liter");
+        System.out.println("\nMasukan Jumlah Galon :");
+        galon = scan.nextDouble();
+        total = galon * 3.785;
+        System.out.println("\nTOTAL ke Liter");
+        System.out.printf("%.0f Galon = %.3f Liter \n\n ", galon, total);
+    }
+}
